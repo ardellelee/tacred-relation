1f639fd9025c3da8011639498e85cd4415114ca3
ardellelee <ardellelee@163.com>
2019-04-18 19:01:45 +0800

    separate stat mode and write mode

--- data/transform_ace_data.py
+++ data/transform_ace_data.py
@@ -6,11 +6,6 @@ Transform ACE data with corenlp annotations to meet requirements of LSTM model.
 History:
     Apri 18, 2019       Initial version.
 
-Usage:
-python -m data.transform_ace_data --input ../relation-extraction-ly-dev/data/ace05_json/ace05_511set/w_head_sys_0131/ace2005.511set.test.nlp.dual.json
-python -m data.transform_ace_data --input ../relation-extraction-ly-dev/data/ace05_json/ace05_511set/w_head_sys_0131/ace2005.511set.validation.nlp.dual.json
-python -m data.transform_ace_data --input ../relation-extraction-ly-dev/data/ace05_json/ace05_511set/w_head_sys_0131/w_gold_mentions/ace2005.511set.train.nlp.dual.json
-
 '''
 import json
 from argparse import ArgumentParser, FileType
@@ -20,70 +15,93 @@ from utils.borrowed.relation_pair import RelationPairGeneratorTransformer
 parser = ArgumentParser()
 parser.add_argument('--input', type=FileType('r'), help='Input ace-style json data')
 parser.add_argument('--output', type=FileType('w'), help='Dump processed data')
+parser.add_argument('--mode', type=str, default='stat', help='Dump processed data')
 args = parser.parse_args()
 
 
 def transform_one(relation_instance):
-    paired_mentions = relation_instance.pop('paired_mentions')
-    annotations = relation_instance.pop('corenlp_annotations')
+  paired_mentions = relation_instance.pop('paired_mentions')
+  annotations = relation_instance.pop('corenlp_annotations')
 
-    m1_span = find_token_span(annotations, paired_mentions['m1'], offsetAdjust=2)
-    m2_span = find_token_span(annotations, paired_mentions['m2'], offsetAdjust=2)
+  m1_span = find_token_span(annotations, paired_mentions['m1'], offsetAdjust=2)
+  m2_span = find_token_span(annotations, paired_mentions['m2'], offsetAdjust=2)
 
-    assert m1_span[0][0] == m1_span[1][0] == m2_span[0][0] == m2_span[1][0]
+  assert m1_span[0][0] == m1_span[1][0] == m2_span[0][0] == m2_span[1][0]
 
-    target_sent = annotations['sentences'][m1_span[0][0]]
+  target_sent = annotations['sentences'][m1_span[0][0]]
 
-    stanford_deprel = [d['dep'] for d in target_sent['enhancedPlusPlusDependencies']]
-    stanford_head = [d['governor'] for d in target_sent['enhancedPlusPlusDependencies']]
-    stanford_ner = [d['ner'] for d in target_sent['tokens']]
-    stanford_pos = [d['pos'] for d in target_sent['tokens']]
-    token = [d['originalText'] for d in target_sent['tokens']]
+  stanford_deprel = [d['dep'] for d in target_sent['basicDependencies']]
+  stanford_head = [d['governor'] for d in target_sent['basicDependencies']]
+  stanford_ner = [d['ner'] for d in target_sent['tokens']]
+  stanford_pos = [d['pos'] for d in target_sent['tokens']]
+  token = [d['originalText'] for d in target_sent['tokens']]
 
-    transformed = dict(docid=relation_instance['docid'],
-                       id=relation_instance['id'],
-                       relation=relation_instance['relation'],
-                       subrelation=relation_instance['subrelation'],
-                       subj_start=m1_span[0][1],
-                       subj_end=m1_span[1][1],
-                       obj_start=m2_span[0][1],
-                       obj_end=m2_span[0][1],
-                       subj_type=paired_mentions['m1']['entity_type'],
-                       obj_type=paired_mentions['m2']['entity_type'],
-                       stanford_deprel=stanford_deprel,
-                       stanford_head=stanford_head,
-                       stanford_ner=stanford_ner,
-                       stanford_pos=stanford_pos,
-                       token=token,
-                       )
+  transformed = dict(docid=relation_instance['docid'],
+                     id=relation_instance['id'],
+                     relation=relation_instance['relation'],
+                     subrelation=relation_instance['subrelation'],
+                     subj_start=m1_span[0][1],
+                     subj_end=m1_span[1][1],
+                     obj_start=m2_span[0][1],
+                     obj_end=m2_span[0][1],
+                     subj_type=paired_mentions['m1']['entity_type'],
+                     obj_type=paired_mentions['m2']['entity_type'],
+                     stanford_deprel=stanford_deprel,
+                     stanford_head=stanford_head,
+                     stanford_ner=stanford_ner,
+                     stanford_pos=stanford_pos,
+                     token=token,
+                     )
 
-    return transformed
+  return transformed
+
+
+def count_stat(values):
+  frequency = {}
+  for v in set(values):
+    frequency[v] = values.count(v)
+  frequency = dict(sorted(frequency.items()))
+  return frequency
 
 
 def main():
 
-    ace_docs = [json.loads(line) for line in args.input]
-    print('%d docments in %s' % (len(ace_docs), args.input.name))
+  ace_docs = [json.loads(line) for line in args.input]
+  print('%d docments in %s' % (len(ace_docs), args.input.name))
 
-    relation_instances = RelationPairGeneratorTransformer(ordered=True).transform(ace_docs)     # args are using the default
-    print('%d relation instances generated.' % len(relation_instances))
+  relation_instances = RelationPairGeneratorTransformer(ordered=True).transform(ace_docs)     # args are using the default
+  print('%d relation instances generated.' % len(relation_instances))
 
-    n_error = 0
-    transformed = []
-    for r in relation_instances:
-        try:
-            transformed.append(transform_one(r))
-        except Exception:
-            n_error += 1
-    # end for
-    if n_error:
-        print('Skipped %d error instances.' % n_error)
+  n_error = 0
+  transformed = []
+  for r in relation_instances:
+    try:
+      transformed.append(transform_one(r))
+    except Exception:
+      n_error += 1
+  # end for
+  if n_error:
+    print('Skipped %d error instances.' % n_error)
 
-    for r in transformed:
-        args.output.write(json.dumps(r, sort_keys=True))
-        args.output.write('\n')
+  if args.mode == 'stat':
+    print('Relation:\n', count_stat([t['relation'] for t in transformed]))
+    print('Subrelation:\n', count_stat([t['subrelation'] for t in transformed]))
+    print('Subject:\n', count_stat([t['subj_type'] for t in transformed]))
+    print('Object:\n', count_stat([t['obj_type'] for t in transformed]))
+
+    all_ner, all_pos, all_deprel = [], [], []
+    for t in transformed:
+      all_ner += t['stanford_ner']
+      all_pos += t['stanford_pos']
+      all_deprel += t['stanford_deprel']
+    print('NER:\n', count_stat(all_ner))
+    print('POS:\n', count_stat(all_pos))
+    print('Deprel:\n', count_stat(all_deprel))
+
+  elif args.mode == 'write':
+    json.dump(transformed, args.output)
     print('Processed data saved to %s' % args.output.name)
 
 
 if __name__ == '__main__':
-    main()
+  main()
--- transform_ace_data.sh
+++ transform_ace_data.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 
 # This is an example script of transforming ACE-style data.
-mkdir dataset/ace/
+
+mkdir dataset/ace05/
 
 train_in="../relation-extraction-ly-dev/data/ace05_json/ace05_511set/w_head_sys_0131/ace2005.511set.train.nlp.dual.json"
-train_out='dataset/ace/ace2005.511set.train.tacred.json'
+train_out='dataset/ace05/train.json'
 
 dev_in="../relation-extraction-ly-dev/data/ace05_json/ace05_511set/w_head_sys_0131/ace2005.511set.validation.nlp.dual.json"
-dev_out='dataset/ace/ace2005.511set.validation.tacred.json'
+dev_out='dataset/ace05/dev.json'
 
 test_in="../relation-extraction-ly-dev/data/ace05_json/ace05_511set/w_head_sys_0131/ace2005.511set.test.nlp.dual.json"
-test_out='dataset/ace/ace2005.511set.test.tacred.json'
+test_out='dataset/ace05/test.json'
 
 
-# process ace data and save
-python -m data.transform_ace_data --input $train_in --output $train_out
-python -m data.transform_ace_data --input $dev_in --output $dev_out
-python -m data.transform_ace_data --input $test_in --output $test_out
+# 'stat' or 'write'
+mode='write'
+echo "==> Training data."
+python -m data.transform_ace_data --input $train_in --output $train_out --mode $mode
+
+echo "==> Dev data."
+python -m data.transform_ace_data --input $dev_in --output $dev_out --mode $mode
+
+echo "==> Test data."
+python -m data.transform_ace_data --input $test_in --output $test_out --mode $mode
 
 echo "==> Done."
